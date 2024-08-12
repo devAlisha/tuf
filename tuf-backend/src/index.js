f56efbcb8ac8d859c5dd34e1b4318c0f0485d4e6
Alisha Singh <alishasingh4546@gmail.com>
2024-08-12 21:28:57 +0530

    Add visitor IP logging and count functionality

--- tuf-backend/src/index.js
+++ tuf-backend/src/index.js
@@ -6,12 +6,36 @@ const bannerRoutes = require("./routes/bannerRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+
 mongoose.connect(process.env.MONGODB_URI);
+
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`Visitor IP: ${ip}`);
+  next();
+});
+
+let visitorCount = 0;
+
+app.use((req, res, next) => {
+  visitorCount++;
+  console.log(`Visitor count: ${visitorCount}`);
+  next();
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/banner", bannerRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong!",
+    error: err.message,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
