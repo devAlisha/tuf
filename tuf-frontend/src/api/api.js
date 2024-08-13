e97a6c012be3caa3e227b78b431026c7481d5a21
Alisha Singh <alishasingh4546@gmail.com>
2024-08-13 19:22:53 +0530

    Update API headers for banner data requests

--- tuf-frontend/src/api/api.js
+++ tuf-frontend/src/api/api.js
@@ -3,11 +3,19 @@ import axios from "axios";
 const API_ENDPOINT = `${import.meta.env.VITE_BACKEND_URI}/api/banner`;
 
 export const fetchBannerData = async () => {
-  const response = await axios.get(API_ENDPOINT);
+  const response = await axios.get(API_ENDPOINT, {
+    headers: {
+      'ngrok-skip-browser-warning': 'true'
+    }
+  });
   return response.data;
 };
 
 export const updateBannerData = async (bannerData) => {
-  const response = await axios.put(API_ENDPOINT, bannerData);
+  const response = await axios.put(API_ENDPOINT, bannerData, {
+    headers: {
+      'ngrok-skip-browser-warning': 'true'
+    }
+  });
   return response.data;
 };
